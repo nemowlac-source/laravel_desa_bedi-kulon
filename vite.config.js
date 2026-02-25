60bc28d7ff68f6cd935e0e2f0801cfb682689b7b
MR_hk <246475482+nemowlac-source@users.noreply.github.com>
2026-02-25 09:18:55 +0700

    akses diHP

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "0.0.0.0", // Mengizinkan akses dari semua perangkat di jaringan yang sama
+        hmr: {
+            host: "192.168.38.125", // Ganti dengan IP laptop kamu yang tadi (Langkah 3) 🛠️
+        },
+    },
 });
